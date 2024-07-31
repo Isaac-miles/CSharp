92113d3ea089957b720331202f32847606172334
isaac-miles <isaacnice247@gmail.com>
2024-08-01 00:11:18 +0100

    conditions

--- WeatherSimulator/WeatherSimulator/Program.cs
+++ WeatherSimulator/WeatherSimulator/Program.cs
@@ -9,6 +9,15 @@
 
             int[] temperature = new int[days];
             string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for(int i=0; i<days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
 
         }
     }

e451f037f6ff6827fda68ab91521feb10c8dd92a
isaac-miles <isaacnice247@gmail.com>
2024-07-31 23:43:30 +0100

    setup

---
+++ CSharp01/Program.cs
@@ -0,0 +1,27 @@
+﻿using CSharp01;
+
+namespace Fundamentals
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seasons seasons = Seasons.Rainy;
+            Arrays.Run();
+
+            switch (seasons)
+            {
+                case Seasons.Rainy:
+                    Console.WriteLine("Its Raining Season already");
+                    break;
+                case Seasons.Hamattan:
+                    Console.WriteLine("Na Hamatan Jare");
+                    break;
+                default:
+                    Console.WriteLine("No Match found try again.");
+                    break;
+            }
+        }
+    }
+}

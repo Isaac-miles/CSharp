8c52049375f2995361caadf52596d65e6a14b599
isaac-miles <isaacnice247@gmail.com>
2024-08-02 00:00:04 +0100

    class OOp

--- ClassApp/obj/Debug/net8.0/ClassApp.AssemblyInfo.cs
+++ ClassApp/obj/Debug/net8.0/ClassApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e00e26043088db9fb94ed514855540ae702163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6cbe6f7f26db71834ff652175f3671b48548a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

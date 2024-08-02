96c4669c377ef8bc43b8b2c7a3a7b17f5aa8d7ff
isaac-miles <isaacnice247@gmail.com>
2024-08-02 01:07:53 +0100

    car

--- ClassApp/Car.cs
+++ ClassApp/Car.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 namespace ClassApp
 {
+
 	public class Car
 	{
-		public Car()
+        string type;
+		string make;
+		int year;
+
+        public Car(string type, string make, int year)
 		{
+			this.type = type;
+			this.make = make;
+			this.year = year;
+
 		}
+
 	}
 }
 

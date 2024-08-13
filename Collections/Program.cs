71c40901b15e60eb48836f139def6214c6f329f1
isaac-miles <isaacnice247@gmail.com>
2024-08-13 20:12:37 +0100

    employee class

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,17 @@
 ﻿namespace Collections;
+class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Salary { get; set; }
+
+    public Employee(string name, int age, int salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+}
 
 class Program
 {
--- Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
+++ Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b35e99839f343b73cf7bf08af4daf55f91e677b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2a5e1d68dbb4e5449561e33dc6bbb600753111d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

5db4e5aecba1b8f8d1656d9eeb73895b3941175b
isaac-miles <isaacnice247@gmail.com>
2024-08-21 23:55:38 +0100

    .

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -6,5 +6,25 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    class Animal<T>
+    {
+        protected virtual void Run(string a) { } 
+        public string Eat()
+        {
+            return "Eating";
+        }
+    }
+
+    class Dog : Animal<String>
+    {
+        public string Bark()
+        {
+            Console.WriteLine("barking");
+           return  this.Eat();
+        }
+
+        protected override void Run(string a) { }
+    }
 }
 

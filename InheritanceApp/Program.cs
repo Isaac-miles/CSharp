edebfc8836c0c0de2b83aaf95a5113cd300d4ae2
isaac-miles <isaacnice247@gmail.com>
2024-08-22 23:28:16 +0100

    .

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -12,6 +12,30 @@ class Program
         void ProcessPayment(decimal amount);
     }
 
- 
+    public class CreditCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Processint payment of " + amount);
+        }
+    }
+
+    public class PayPalProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Processing paypal payment of " + amount);
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IPaymentProcessor _processor;
+
+        public PaymentService(IPaymentProcessor processor)
+        {
+            _processor = processor;
+        }
+    }
 }
 

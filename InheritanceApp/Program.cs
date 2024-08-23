21dfd7af9c557e345164a0072b5020f0ab2b86fb
isaac-miles <isaacnice247@gmail.com>
2024-08-23 18:25:14 +0100

    .

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -1,52 +1,14 @@
-﻿namespace InheritanceApp;
-
-class Program
+﻿using System;
+namespace InheritanceApp
 {
-
-    public interface IPaymentProcessor
-    {
-        void ProcessPayment(decimal amount);
-    }
-
-    public class CreditCardProcessor : IPaymentProcessor
-    {
-        public void ProcessPayment(decimal amount)
-        {
-            Console.WriteLine("Processint payment of " + amount);
-        }
-    }
-
-    public class PayPalProcessor : IPaymentProcessor
-    {
-        public void ProcessPayment(decimal amount)
-        {
-            Console.WriteLine("Processing paypal payment of " + amount);
-        }
-    }
-
-    public class PaymentService
-    {
-        private readonly IPaymentProcessor _processor;
-
-        public PaymentService(IPaymentProcessor processor)
-        {
-            _processor = processor;
-        }
-
-        public void ProcessOrderPayment(decimal amount)
-        {
-            _processor.ProcessPayment(amount);
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        IPaymentProcessor processPay = new PayPalProcessor();
-        processPay.ProcessPayment(10);
-
-        PaymentService servicePay = new PaymentService(processPay);
-        servicePay.ProcessOrderPayment(15.0m);
-    }
-
+	public class Program
+	{
+		 static void Main(string[] args)
+		{
+			File.AppendAllText("log.txt", "miles" + "\n");
+			Console.ReadLine();
+		}
+		
+	}
 }
 
---
+++ InheritanceApp/ProgramInterface.cs
@@ -0,0 +1,52 @@
+﻿namespace InheritanceApp;
+
+class ProgramInterface
+{
+
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Processint payment of " + amount);
+        }
+    }
+
+    public class PayPalProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            //Console.WriteLine("Processing paypal payment of " + amount);x
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IPaymentProcessor _processor;
+
+        public PaymentService(IPaymentProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public void ProcessOrderPayment(decimal amount)
+        {
+            _processor.ProcessPayment(amount);
+        }
+    }
+
+    //static void Main(string[] args)
+    //{
+    //    IPaymentProcessor processPay = new PayPalProcessor();
+    //    processPay.ProcessPayment(10);
+
+    //    PaymentService servicePay = new PaymentService(processPay);
+    //    servicePay.ProcessOrderPayment(15.0m);
+    //}
+
+}
+

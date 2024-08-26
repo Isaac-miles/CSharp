888fd0d182bd4da6404a8ab11f8062a96d2bda78
isaac-miles <isaacnice247@gmail.com>
2024-08-26 22:49:42 +0100

    .

--- InheritanceApp/DelegateClass.cs
+++ InheritanceApp/DelegateClass.cs
@@ -3,8 +3,21 @@ namespace InheritanceApp
 {
 	public class DelegateClass
 	{
+		public delegate void Notify(string message);
+
 		public DelegateClass()
 		{
+			//instantiation
+			Notify notifyDelegate = ShowMessage;
+
+			//invocation
+
+
+		}
+
+		static void ShowMessage(string message)
+		{
+			Console.WriteLine(message);
 		}
 	}
 }

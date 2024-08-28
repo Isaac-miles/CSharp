5fe40b74cc5947481a25ac5fc8a0d4ad6b0eb3cc
isaac-miles <isaacnice247@gmail.com>
2024-08-28 13:12:18 +0100

    .

--- InheritanceApp/EventsClass.cs
+++ InheritanceApp/EventsClass.cs
@@ -10,17 +10,25 @@ namespace InheritanceApp
             Console.WriteLine("Event raised: "+ message);
         }
     }
-  
 
-    public class EventsClass
-	{
+    class EventPublisher
+    {
         public event Notify OnNotify;
 
         public void RaiseEvent(string message)
         {
             OnNotify.Invoke(message);
         }
+    }
 
-	}
+    public class EventsClass
+	{
+        static void Main(string[] args)
+        {
+            EventPublisher publiser = new EventPublisher();
+            EventSubscribe subscribe = new EventSubscribe();
+        }
+    }
+
 }
 

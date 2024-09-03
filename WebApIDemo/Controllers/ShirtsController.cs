32330e3c103a49d0f2783567d89dd13f41289c97
isaac-miles <isaacnice247@gmail.com>
2024-09-04 00:13:14 +0100

    add services

--- WebApIDemo/Controllers/ShirtsController.cs
+++ WebApIDemo/Controllers/ShirtsController.cs
@@ -13,22 +13,30 @@ namespace WebApIDemo.Controllers
             return "Get all Shirts";
         }
 
+        [HttpGet]
+        [Route("/shirts/{id}")]
         public string GetShirt(int ID)
         {
             return "Get the Shirts with ID:" + ID;
         }
 
+        [HttpPost]
+        [Route("/shirts")]
         public string CreateShirts()
         {
             return "Creating a shirt";
         }
 
+        [HttpPut]
+        [Route("/shirts/{id}")]
         public string UpdatingShirt(int ID)
         {
             return "Updating the Shirts with ID:" + ID;
 
         }
 
+        [HttpDelete]
+        [Route("/shirts/{id}")]
         public string DeletingShirt(int ID)
         {
             return "Deleting the Shirts with ID:" + ID;

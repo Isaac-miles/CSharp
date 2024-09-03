0dcdce4ffed91a54f51d8b7015c902ae62fd9ddd
isaac-miles <isaacnice247@gmail.com>
2024-09-03 23:30:00 +0100

    shirt controller

--- WebApIDemo/Controllers/ShirtsController.cs
+++ WebApIDemo/Controllers/ShirtsController.cs
@@ -6,10 +6,32 @@ namespace WebApIDemo.Controllers
 	[ApiController]
 	public class ShirtsController: ControllerBase
 	{
-		public ShirtsController()
-		{
+        public string GetAllShirts()
+        {
+            return "Get all Shirts";
+        }
 
-		}
-	}
+        public string GetShirt(int ID)
+        {
+            return "Get the Shirts with ID:" + ID;
+        }
+
+        public string CreateShirts()
+        {
+            return "Creating a shirt";
+        }
+
+        public string UpdatingShirt(int ID)
+        {
+            return "Updating the Shirts with ID:" + ID;
+
+        }
+
+        public string DeletingShirt(int ID)
+        {
+            return "Deleting the Shirts with ID:" + ID;
+
+        }
+    }
 }
 

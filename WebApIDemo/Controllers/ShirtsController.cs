3f33d012a34fb357adf6c621597dde3fd9833c76
isaac-miles <isaacnice247@gmail.com>
2024-09-04 21:54:29 +0100

    shirt repo

--- WebApIDemo/Controllers/ShirtsController.cs
+++ WebApIDemo/Controllers/ShirtsController.cs
@@ -8,21 +8,12 @@ namespace WebApIDemo.Controllers
     [Route("api/[controller]")] //with this you dont need to add Route individually
 	public class ShirtsController: ControllerBase
 	{
-        private List<Shirt> shirts = new List<Shirt>()
-        {
-            new Shirt {ShirtId=1,Brand="Viton",Color="Blue",Gender="Men",Price=30,Size=10},
-            new Shirt {ShirtId=2,Brand="Luios",Color="Green",Gender="Men",Price=20,Size=12},
-            new Shirt {ShirtId=3,Brand="Xena",Color="Grey",Gender="Women",Price=15,Size=8},
-            new Shirt {ShirtId=4,Brand="Okrika",Color="Pink",Gender="Women",Price=30,Size=9},
-
-        };
-
-
+      
         [HttpGet]
         //[Route("/shirts")]
-        public string GetAllShirts()
+        public IActionResult GetAllShirts()
         {
-            return "Get all Shirts";
+            return Ok("Get all Shirts");
         }
 
         [HttpGet("{id}")]
@@ -32,7 +23,13 @@ namespace WebApIDemo.Controllers
         public IActionResult GetShirt(int id)
         {
             //use the IActionResult return type when your method returns different type of data
-            Shirt shirt = shirts.First(x => x.ShirtId == id);
+
+            if(id <= 0)
+            {
+                return BadRequest("Invalid Id");
+            }
+            var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+
             if(shirt == null)
             {
                 return NotFound();
@@ -43,24 +40,24 @@ namespace WebApIDemo.Controllers
 
         [HttpPost]
         //[Route("/shirts")]
-        public Shirt CreateShirts( [FromBody] Shirt shirt)
+        public IActionResult CreateShirts( [FromBody] Shirt shirt)
         {
-            return shirt;
+            return Ok(shirt);
         }
 
         [HttpPut("{id}")]
         //[Route("/shirts/{id}")]
-        public string UpdatingShirt(int ID)
+        public IActionResult UpdatingShirt(int ID)
         {
-            return "Updating the Shirts with ID:" + ID;
+            return Ok("Updating the Shirts with ID:" + ID);
 
         }
 
         [HttpDelete("{id}")]
         //[Route("/shirts/{id}")]
-        public string DeletingShirt(int ID)
+        public IActionResult DeletingShirt(int ID)
         {
-            return "Deleting the Shirts with ID:" + ID;
+            return Ok("Deleting the Shirts with ID:" + ID);
 
         }
     }
---
+++ WebApIDemo/Models/Repository/ShirtsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebApIDemo.Models.Repository
+{
+	public static class ShirtsRepository
+	{
+        private static List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt {ShirtId=1,Brand="Viton",Color="Blue",Gender="Men",Price=30,Size=10},
+            new Shirt {ShirtId=2,Brand="Luios",Color="Green",Gender="Men",Price=20,Size=12},
+            new Shirt {ShirtId=3,Brand="Xena",Color="Grey",Gender="Women",Price=15,Size=8},
+            new Shirt {ShirtId=4,Brand="Okrika",Color="Pink",Gender="Women",Price=30,Size=9},
+
+        };
+
+    }
+}
+

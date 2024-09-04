992821b7ccedf3a2b911dc54e803a09dabbd323a
isaac-miles <isaacnice247@gmail.com>
2024-09-04 21:10:49 +0100

    custom validation attribute

--- WebApIDemo/Models/Validations/Shirt_EnsureCorrectSizingAttribute.cs
+++ WebApIDemo/Models/Validations/Shirt_EnsureCorrectSizingAttribute.cs
@@ -7,7 +7,21 @@ namespace WebApIDemo.Models.Validations
 	{
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            return base.IsValid(value, validationContext);
+            var shirt = validationContext.ObjectInstance as Shirt;
+            if(shirt != null && !string.IsNullOrWhiteSpace(shirt.Gender))
+            {
+                if (shirt.Gender.Equals("men", StringComparison.OrdinalIgnoreCase) && shirt.Size < 8)
+                {
+                    return new ValidationResult("For Men's shirt, the size has to be greater or equal to 8");
+                }
+                else if (shirt.Gender.Equals("women", StringComparison.OrdinalIgnoreCase) && shirt.Size < 6)
+                {
+                    return new ValidationResult("For Women's shirt, the size has to be greater or equal to 6");
+
+                }
+            }
+
+            return ValidationResult.Success;
         }
     }
 }

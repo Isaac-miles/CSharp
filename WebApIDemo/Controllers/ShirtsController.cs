37afe74f38e7cb5219dd4d083e3454c2ddb164a2
isaac-miles <isaacnice247@gmail.com>
2024-09-04 21:33:47 +0100

    handled exceptions

--- WebApIDemo/Controllers/ShirtsController.cs
+++ WebApIDemo/Controllers/ShirtsController.cs
@@ -8,6 +8,16 @@ namespace WebApIDemo.Controllers
     [Route("api/[controller]")] //with this you dont need to add Route individually
 	public class ShirtsController: ControllerBase
 	{
+        private List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt {ShirtId=1,Brand="Viton",Color="Blue",Gender="Men",Price=30,Size=10},
+            new Shirt {ShirtId=2,Brand="Luios",Color="Green",Gender="Men",Price=20,Size=12},
+            new Shirt {ShirtId=3,Brand="Xena",Color="Grey",Gender="Women",Price=15,Size=8},
+            new Shirt {ShirtId=4,Brand="Okrika",Color="Pink",Gender="Women",Price=30,Size=9},
+
+        };
+
+
         [HttpGet]
         //[Route("/shirts")]
         public string GetAllShirts()
@@ -19,9 +29,16 @@ namespace WebApIDemo.Controllers
         //[Route("/shirts/{id}/{color}")]
         //[FromQuery] string color / [FromRoute] string color  [FromHeader(Name ="color")] string color
 
-        public string GetShirt(int id)
+        public IActionResult GetShirt(int id)
         {
-            return $"Get the Shirts with ID: {id}";
+            //use the IActionResult return type when your method returns different type of data
+            Shirt shirt = shirts.First(x => x.ShirtId == id);
+            if(shirt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shirt);
         }
 
         [HttpPost]

1f519bc9a93e4b0a0edd7364ac04374a9ef1a0f3
David Isaac <david.omokafe@fidelitybank.ng>
2024-09-05 23:11:27 +0100

    coupon list

---
+++ Data/CouponStore.cs
@@ -0,0 +1,13 @@
+﻿using Web_Minimal_API.Models;
+
+namespace Web_Minimal_API.Data
+{
+    public class CouponStore
+    {
+        public static List<Coupon> CouponList = new List<Coupon>()
+        {
+            new Coupon{Id=1, Name="1F00CH",Percent=10, IsActive=true},
+            new Coupon{Id=2,Name="200BYT", Percent=30, IsActive=false}
+        };
+    }
+}

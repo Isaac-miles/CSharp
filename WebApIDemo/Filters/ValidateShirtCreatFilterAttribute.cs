fc572bb479c0280157226e3bae6e93c42bd53f70
isaac-miles <isaacnice247@gmail.com>
2024-09-05 10:51:34 +0100

    onexecuting context

--- WebApIDemo/Filters/ValidateShirtCreatFilterAttribute.cs
+++ WebApIDemo/Filters/ValidateShirtCreatFilterAttribute.cs
@@ -5,7 +5,10 @@ namespace WebApIDemo.Filters
 {
 	public class ValidateShirtCreatFilterAttribute: ActionFilterAttribute
 	{
-		
-	}
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+    }
 }
 

101605e8c1804966400eeb9367633ed5e2dcdf98
isaac-miles <isaacnice247@gmail.com>
2024-09-13 23:02:32 +0100

    .

--- WebApIDemo/obj/Debug/net8.0/WebApIDemo.AssemblyInfo.cs
+++ WebApIDemo/obj/Debug/net8.0/WebApIDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApIDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b9f9bc6fd0ecf14a2ce20d8630f22b1b6337ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2160d277b0209864fc6f23a8d65cc07399845f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApIDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApIDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

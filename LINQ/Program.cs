68ebee8aef2ee34e8c91f459028b8e9afaa8f19b
isaac-miles <isaacnice247@gmail.com>
2024-09-19 21:54:27 +0100

    .

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
         Console.WriteLine("Hello, World!");
     }
+
+    static void OddNumbers(int[] numbers)
+    {
+
+    }
+        
 }
 

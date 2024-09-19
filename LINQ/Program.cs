7cd1f7b27fe98472b97b0f2f35975d572c76a386
isaac-miles <isaacnice247@gmail.com>
2024-09-19 22:05:52 +0100

    student class

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -30,7 +30,29 @@ class Program
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("University {0} with Id {1} ", Name, Id);
+
+        }
     }
 
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        //Foreign Key
+        public int UniversityId { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("University {0} with Id {1} ", Name, Id);
+
+        }
+    }
 }
 

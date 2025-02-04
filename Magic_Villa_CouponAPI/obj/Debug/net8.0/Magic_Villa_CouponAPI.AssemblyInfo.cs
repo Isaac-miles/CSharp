89f3989a6dbdefe4b2114b37a8f75bac9356d407
isaac-miles <isaacnice247@gmail.com>
2025-02-04 20:32:27 +0100

    update

--- Magic_Villa_CouponAPI/obj/Debug/net8.0/Magic_Villa_CouponAPI.AssemblyInfo.cs
+++ Magic_Villa_CouponAPI/obj/Debug/net8.0/Magic_Villa_CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magic_Villa_CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0452d7872599b2ea16518c32e7a335f7ec028f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0433c75865fba95575d08562c569740517ad743")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magic_Villa_CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magic_Villa_CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
